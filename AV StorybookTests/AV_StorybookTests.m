8e3d68c05f4413bc26e890832e8f01616ac66950
asu <arsenyk.ustaris.dev@gmail.com>
2015-09-11 14:31:25 -0700

    Initial Commit

---
+++ AV StorybookTests/AV_StorybookTests.m
@@ -0,0 +1,40 @@
+//
+//  AV_StorybookTests.m
+//  AV StorybookTests
+//
+//  Created by asu on 2015-09-11.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AV_StorybookTests : XCTestCase
+
+@end
+
+@implementation AV_StorybookTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
